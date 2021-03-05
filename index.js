b2ecf8cd83da78ec9b98988b68bfb88225e1d40f
ndev97 <60653247+ndev97@users.noreply.github.com>
2021-03-05 04:35:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+let jugadores = document.querySelector("#ingresar");
+let generar = document.querySelector("#enviar");;
+
+let min = 1;
+let max = 3;
+let NumeroTeam;
+
+let playersArray = new Array(10);
+
+jugadores.addEventListener("click", ()=>{
+
+    for(i = 0; i < 10; i++){
+        playersArray[i] = prompt("Ingrese jugador", `jugador ${i+1}`);
+    }
+
+    document.getElementById("jugador1").value = playersArray[0];
+    document.getElementById("jugador2").value = playersArray[1];
+    document.getElementById("jugador3").value = playersArray[2];
+    document.getElementById("jugador4").value = playersArray[3];
+    document.getElementById("jugador5").value = playersArray[4];
+    document.getElementById("jugador6").value = playersArray[5];
+    document.getElementById("jugador7").value = playersArray[6];
+    document.getElementById("jugador8").value = playersArray[7];
+    document.getElementById("jugador9").value = playersArray[8];
+    document.getElementById("jugador10").value = playersArray[9];
+
+});
+
+generar.addEventListener("click", () =>{
+
+    let i = playersArray.length;
+    let numRandom;
+    let temp;
+
+    while(--i > 0){
+        numRandom = Math.floor(Math.random() * (i+1));
+        temp = playersArray[numRandom];
+        playersArray[numRandom] = playersArray[i];
+        playersArray[i] = temp;
+    };
+
+    document.getElementById("jugador1").value = playersArray[0];
+    document.getElementById("jugador2").value = playersArray[1];
+    document.getElementById("jugador3").value = playersArray[2];
+    document.getElementById("jugador4").value = playersArray[3];
+    document.getElementById("jugador5").value = playersArray[4];
+    document.getElementById("jugador6").value = playersArray[5];
+    document.getElementById("jugador7").value = playersArray[6];
+    document.getElementById("jugador8").value = playersArray[7];
+    document.getElementById("jugador9").value = playersArray[8];
+    document.getElementById("jugador10").value = playersArray[9];
+});
+
